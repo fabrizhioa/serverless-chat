eb7f8cedc6262838348a1f880aacd2e69991c473
Fabrizhio Al chariti <35412190+fabrizhioa@users.noreply.github.com>
2022-01-25 10:13:27 -0400

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,13 @@
+//importar paquete
 const express = require('express');
+
+//importar modelo
 const Users = require('../models/Users');
 
+//iniciar Ruta
 const router = express.Router();
 
+//accciones validas
 router.get('/:email', (req, res) => {
     Users.find({ email: req.params.email })
         .exec()
@@ -13,4 +18,5 @@ router.post('/', (req, res) => {
     Users.create(req.body).then((x) => res.status(201).send(x));
 });
 
+//exportar ruta
 module.exports = router;
