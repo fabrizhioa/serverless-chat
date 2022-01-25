b3b76aa5b58962ce3fad6825a2706bcc1f5fda2e
Fabrizhio Al chariti <35412190+fabrizhioa@users.noreply.github.com>
2022-01-25 10:10:22 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,24 +1,33 @@
+//importar paquetes requeridos
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-//const users = require('./routes/users');
+//importar rutas
 const messages = require('./routes/messages');
 const users = require('./routes/users');
 
+//iniciar servidor
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(cors());
 
+//conexion con base de datos
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+//rutas validas de la api
+
 app.use('/api/messages', messages);
+
 app.use('/api/users', users);
+
+//exportar aplicacion
 
 module.exports = app;
